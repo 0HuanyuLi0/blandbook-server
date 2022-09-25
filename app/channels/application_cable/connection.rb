2e8461c26ad4b01885fff8a51e2f54dd02b783d8
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2022-09-25 17:47:37 +1000

    Removed  connection for deploy

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,31 +1,31 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
    
-    #On connection, ensure that the user is the correct one.
+#     #On connection, ensure that the user is the correct one.
  
-  def connect   
+#   def connect   
 
-    find_verified_user 
-  end
+#     find_verified_user 
+#   end
  
-private
+# private
 
-  # ! Thanks to  https://itnext.io/actioncable-authentication-in-a-token-based-rails-api-f9cc4b8bf560
+#   # ! Thanks to  https://itnext.io/actioncable-authentication-in-a-token-based-rails-api-f9cc4b8bf560
 
 
-def find_verified_user   
+# def find_verified_user   
 
-  token = request.headers[:HTTP_SEC_WEBSOCKET_PROTOCOL].split(" ").last
-  decoded_token = JWT.decode token, "secret", true, { :algorithm => "HS256" }
-  p "decoded-token", decoded_token
-  @current_user = User.find((decoded_token[0])["sub"])
-    if @current_user.present?
+#   token = request.headers[:HTTP_SEC_WEBSOCKET_PROTOCOL].split(" ").last
+#   decoded_token = JWT.decode token, "secret", true, { :algorithm => "HS256" }
+#   p "decoded-token", decoded_token
+#   @current_user = User.find((decoded_token[0])["sub"])
+#     if @current_user.present?
       
-      @current_user
-    else
-      reject_unauthorized_connection
-    end
-  end
+#       @current_user
+#     else
+#       reject_unauthorized_connection
+#     end
+  # end
 
  
   end #end method
